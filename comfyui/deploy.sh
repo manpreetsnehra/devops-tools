8dab69dc285035c867a457a15c93be104bb5901f
Manpreet Singh Nehra <manpreet@nehraconsultancy.net>
2026-07-16 23:44:50 +0300

    Pipeline overhaul to incorporate pull requests and helm relases (#4)
    * overhaul all pipelines
    
    * New docker image template
    
    * split job into 2 and run strategy
    
    * Update codeserver docker
    
    * Merge all pipelines into 1 template
    
    * Fix Secrets input
    
    * Fix Secrets input
    
    * Fix helm pipline
    
    * Fix helm pipline 02
    
    * Push on tags only
    
    * Push on tags only
    
    * Split piipelines into 2, one for publishing one for personal
    
    * Split piipelines into 2, one for publishing one for personal
    
    * Split piipelines into 2, one for publishing one for personal
    
    * Fix pipelines syntax 03
    
    * Fix pipelines syntax 04
    
    * Fix pipelines syntax 05
    
    * Fix pipelines syntax 06
    
    * Fix pipelines syntax 07
    
    * Fix pipelines syntax 08
    
    * Fix pipelines syntax 09: Remove concurrency conditions
    
    * Fix pipelines syntax 10
    
    * Fix pipelines syntax 11
    
    * Fix pipelines syntax 12
    
    * Fix pipelines syntax 12
    
    * Fix pipelines syntax 14: Ignore dirs for non deploy pipeline
    
    * Fix pipelines syntax 15: Ignore dirs for non deploy pipeline
    
    * Cleanup devops
    
    * chmod as root
    
    * Trim condition for pipeline
    
    * Don't run deploy pipeline on every push
    
    * ADd missing helmfile
    
    * Tweak pipeline errors and cleanup deploy dockerfilee
    
    * Change chehk for changed dir, add missing option
    
    * Change chehk for changed dir, add missing option
    
    * Cleanup pipelines for docker image build
    
    * Cleanup pipelines for docker image build 02
    
    * Cleanup pipelines for docker image build 02
    
    * Fix condition and incorrect variiables
    
    * Enable security scans for all images
    
    * Debug outputs
    
    * Debug outputs 033
    
    * Debug env
    
    * Fix outputs
    
    * Integrate Version requests into docker buiilds
    
    * Add default value for arg
    
    * Add default value for arg
    
    * remove dyynamimc labels
    
    * remove dyynamimc labels
    
    * Fix helm chart publishing pipelines
    
    * Helm pipielinee cleanup and security update to Pr
    
    * Helm pipielinee cleanup and security update to Pr
    
    * push requried for testing images
    
    * Always push images
    
    * Fiix deb file name
    
    * Use exact file names
    
    * Remove debug bash options
    
    * Remove debug bash options
    
    * Debug helm chart list
    
    * Checkc in charts dir
    
    * Fix matrix
    
    * Add charts dir
    
    * Add charts dir
    
    * Allow display of security results
    
    * Allow display of security results
    
    * Try js output for PR update
    
    * Post output to summary
    
    * Helm chart test
    
    * Update codeserver and add chart testing
    
    * Update codeserver and add chart testing
    
    * Fixx pr pipelines
    
    * Fix helmfile pr and disable trivy for deploy for now
    
    * Fix helmfile pr and disable trivy for deploy for now
    
    * Fix image pr checks and helm pr
    
    * Update whisparr base image
    
    * Update comfyui-base
    
    * cleanup pr extra build-args
    
    * Test new PR updation
    
    * Test new PR updation 02
    
    * Test new PR updation 03
    
    * Test new PR updation 04
    
    * Focus on High and critical results
    
    * Upload security results to github security tab
    
    * Debug matrix source
    
    * Helm testing does not need changed files check
    
    * Fix deploy image name for security cehck and test helm ct
    
    * Fix command for lint
    
    * No working dir for helm chart testing needed
    
    * No working dir for helm chart testing needed
    
    * Add maintainers for chart
    
    * Debug push change detection and add extra line at end of file
    
    * Debug push change detection and add extra line at end of file 02
    
    * Update debug for testing helm chart and mainerns
    
    * Update debug for testing helm chart and mainerns 02
    
    * Disable Chart publishing

---
+++ comfyui/deploy.sh
@@ -0,0 +1,62 @@
+#!bin/bash
+
+DEPLOY_DIR=${INSTALL_DIR}/deploy
+
+mkdir -p $DEPLOY_DIR $MODELS_DIR
+
+MODEL_DIRECTORIES=(
+    "checkpoints"
+    "clip"
+    "clip_vision"
+    "configs"
+    "controlnet"
+    "diffusers"
+    "diffusion_models"
+    "embeddings"
+    "gligen"
+    "hypernetworks"
+    "loras"
+    "photomaker"
+    "style_models"
+    "text_encoders"
+    "unet"
+    "upscale_models"
+    "vae"
+    "vae_approx"
+)
+for MODEL_DIRECTORY in ${MODEL_DIRECTORIES[@]}; do
+    mkdir -p ${BASE_DIR}/models/
+done
+
+## Get Versions
+
+## Get ComfyUI
+if [[ ! -d ${DEPLOY_DIR}/ui ]] || [[ $COMFYUI_VERSION != $(cat ${INSTALL_DIR}/COMFYUI_INSTALLED_VERSION) ]]
+then
+  mkdir -p ${DEPLOY_DIR}/ui
+  curl -s -L https://github.com/Comfy-Org/ComfyUI/archive/refs/tags/v${COMFYUI_VERSION}.tar.gz | tar xfz - -C ${DEPLOY_DIR}/ui
+  echo $COMFUI_VERSION > ${INSTALL_DIR}/COMFYUI_INSTALLED_VERSION
+fi
+
+## Get ComfyUI Distributed
+if [[ ! -d ${BASE_DIR}/custom_nodes/ComfyUI-Distributed ]] || [[ $COMFYUI_DISTRIBUTED_VERSION != $(cat ${INSTALL_DIR}/COMFYUI_DISTRIBUTED_INSTALLED_VERSION) ]]
+then
+  mkdir -p ${BASE_DIR}/custom_nodes/ComfyUI-Distributed
+  curl -s -L https://github.com/robertvoy/ComfyUI-Distributed/archive/refs/tags/v${COMFYUI_DISTRIBUTED_VERSION}.tar.gz | tar xfz - -C ${BASE_DIR}/custom_nodes/ComfyUI-Distributed
+  echo $COMFUI_DISTRIBUTED_VERSION > ${INSTALL_DIR}/COMFYUI_DISTRIBUTED_INSTALLED_VERSION  
+fi
+
+## Setup Virtual Env
+virtualenv ${DEPLOY_DIR}/venv
+
+## Activate Python Virtual ENV
+source ${DEPLOY_DIR}/venv/bin/activate
+
+## Install Pytorch
+if [[ $1 == 'amd' ]]
+then
+  pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm${ROCM_VERSION}
+  pip install --requirement ${DEPLOY_DIR}/ui/requirements.txt --requirement ${DEPLOY_DIR}/ui/manager_requirements.txt
+else
+  pip install --requirement ${DEPLOY_DIR}/ui/requirements.txt --requirement ${DEPLOY_DIR}/ui/manager_requirements.txt --extra-index-url https://download.pytorch.org/whl/${CUDA_VERSION}
+fi  
