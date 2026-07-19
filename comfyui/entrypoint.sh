f2b8f2282e5e2dc5a10b555c5bc10467407b8167
Manpreet Singh Nehra <6732864+manpreetsnehra@users.noreply.github.com>
2026-07-19 21:55:16 +0300

    Helm charts pipelines and docker image updates (#17)
    * Update comfyui docker imaige and fix actions runner sudo
    
    * Update comfyui docker imaige and fix actions runner sudo
    
    * Download and incorporate the comfyui code into  image
    
    * Remove verrbosity from untar
    
    * Fix file name
    
    * Remove templates and enable helm pipelines
    
    * Remove templates and enable helm pipelines
    
    * Update chart version
    
    * Fix chart comments
    
    * Add requirred spaces
    
    * Remove Trailing spaces
    
    * Rename Chart for comfyui-master
    
    * Test chart against 4 kind versions
    
    * Env install dir is always needed
    
    * Enable probes
    
    * Updaate the pipeleine for kind versions
    
    * Fix version for kind cluster version
    
    * Disable other clusters
    
    * Disable other clusters
    
    * Enable other clusters
    
    * Don't you chart testing to test docker image
    
    * Don't you chart testing to test docker image
    
    * Don't you chart testing to test docker image
    
    * Don't you chart testing to test docker image
    
    * Don't you chart testing to test docker image
    
    * Don't you chart testing to test docker image
    
    * Test chart with corrrect image
    
    * only on specific branches
    
    * Test chart with corrrect image
    
    * remove install dir
    
    * Update docker image
    
    * Update hart name
    
    * Updaate chart to ignore values
    
    * Skip error for comfyui
    
    * just ignore comyfui charts
    
    * Revert name
    
    * Enable init-chmod permissions
    
    * readonly root fs
    
    * Lint cleanup

---
+++ comfyui/entrypoint.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/bash -x
+
+### Set Vars
+if [[ -z "${BASE_DIR}" ]]; then BASE_DIR=/home/comfy/ComfyUI; fi
+if [[ ! -z "${TEMP_DIR}" ]];then CUSTOM_TEMP="--temp-directory $TEMP_DIR"; fi
+if [[ ! -z "${INPUT_DIR}" ]];then CUSTOM_INPUT="--input-directory ${INPUT_DIR}";fi
+if [[ ! -z "${OUTPUT_DIR}" ]];then CUSTOM_OUTPUT="--input-directory ${OUTPUT_DIR}";fi
+if [[ ! -z "${USER_DIR}" ]];then CUSTOM_USER="--input-directory ${USER_DIR}";fi
+
+mkdir -p $BASE_DIR $INPUT_DIR $TEMP_DIR $OUTPUT_DIR $USER_DIR
+unzip -qq /home/comfy/comfyui.zip 
+cp -a ComfyUI-master/* $BASE_DIR
+
+mkdir -p ${BASE_DIR}/custom_nodes/ComfyUI-Distributed
+tar xfz /home/comfy/comfyui-distributed.tar.gz -C ${BASE_DIR}/custom_nodes/ComfyUI-Distributed --strip-components=1
+
+## Setup Virtual Env
+virtualenv ${BASE_DIR}/.venv
+
+## Install Pytorch
+if [[ $GPU_TYPE == 'amd' ]]
+then
+  ${BASE_DIR}/.venv/bin/pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm${ROCM_VERSION}
+  ${BASE_DIR}/.venv/bin/pip install --requirement ${BASE_DIR}/requirements.txt --requirement ${BASE_DIR}/manager_requirements.txt
+elif [[ $GPU_TYPE == 'nvidia' ]]
+then
+  ${BASE_DIR}/.venv/bin/pip install --requirement ${BASE_DIR}/requirements.txt --requirement ${BASE_DIR}/manager_requirements.txt --extra-index-url https://download.pytorch.org/whl/${CUDA_VERSION}
+else
+  ${BASE_DIR}/.venv/bin/pip install --requirement ${BASE_DIR}/requirements.txt --requirement ${BASE_DIR}/manager_requirements.txt
+fi  
+
+${BASE_DIR}/.venv/bin/pip install matrix-nio
+
+if [[ $GPU_TYPE == 'amd' ]] || [[ $GPU_TYPE == 'nvidia' ]]
+then
+    ${BASE_DIR}/.venv/bin/python ${BASE_DIR}/main.py \
+        --enable-cors-header \
+        --disable-auto-launch \
+        ${CUSTOM_TEMP} \
+        ${CUSTOM_INPUT} \
+        ${CUSTOM_OUTPUT} \
+        ${CUSTOM_USER} \
+        --enable-manager \
+        --listen 127.0.0.1 \
+        --port 8188
+else
+    ${BASE_DIR}/.venv/bin/python ${BASE_DIR}/main.py \
+        --enable-cors-header \
+        --disable-auto-launch \
+        ${CUSTOM_TEMP} \
+        ${CUSTOM_INPUT} \
+        ${CUSTOM_OUTPUT} \
+        ${CUSTOM_USER} \
+        --enable-manager \
+        --cpu \
+        --listen 127.0.0.1 \
+        --port 8188
+fi
