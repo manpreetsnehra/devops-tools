2b65a9179fdd98a6ce707908ee9d4c1dd5255511
Manpreet Singh Nehra <6732864+manpreetsnehra@users.noreply.github.com>
2026-07-23 13:34:50 +0300

    Comfy with nodes (#38)
    * Update changelog and update comfyui
    
    * put code in but install venv later
    
    * Tweak to include custom nodes
    
    * Add nodes to docker image
    
    * Cleanup code duplications

--- comfyui/entrypoint.sh
+++ comfyui/entrypoint.sh
@@ -1,44 +1,37 @@
 #!/usr/bin/bash -x
 
 ### Set Vars
-if [[ -z "${BASE_DIR}" ]]; then BASE_DIR=/home/comfy/ComfyUI; fi
+if [[ -z "${BASE_DIR}" ]]; then BASE_DIR=/home/comfy/ComfyUI-master; fi
 if [[ ! -z "${TEMP_DIR}" ]];then CUSTOM_TEMP="--temp-directory $TEMP_DIR"; fi
 if [[ ! -z "${INPUT_DIR}" ]];then CUSTOM_INPUT="--input-directory ${INPUT_DIR}";fi
 if [[ ! -z "${OUTPUT_DIR}" ]];then CUSTOM_OUTPUT="--input-directory ${OUTPUT_DIR}";fi
 if [[ ! -z "${USER_DIR}" ]];then CUSTOM_USER="--input-directory ${USER_DIR}";fi
 
-mkdir -p $BASE_DIR $INPUT_DIR $TEMP_DIR $OUTPUT_DIR $USER_DIR
-cp -a ComfyUI-master/* $BASE_DIR
-rm ComyUI-master
-
-mkdir -p ${BASE_DIR}/custom_nodes/ComfyUI-Distributed
-tar xfz /home/comfy/comfyui-distributed.tar.gz -C ${BASE_DIR}/custom_nodes/ComfyUI-Distributed --strip-components=1
-
 ## Setup Virtual Env
-virtualenv ${BASE_DIR}/.venv
+virtualenv ${HOME}/.venv
 
 ## Install Pytorch
 if [[ $GPU_TYPE == 'amd' ]]
 then
-  ${BASE_DIR}/.venv/bin/pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm${ROCM_VERSION}
-  ${BASE_DIR}/.venv/bin/pip install --requirement ${BASE_DIR}/requirements.txt --requirement ${BASE_DIR}/manager_requirements.txt
+  ${HOME}/.venv/bin/pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm${ROCM_VERSION}
+  ${HOME}/.venv/bin/pip install --requirement ${BASE_DIR}/requirements.txt --requirement ${BASE_DIR}/manager_requirements.txt
 elif [[ $GPU_TYPE == 'nvidia' ]]
 then
-  ${BASE_DIR}/.venv/bin/pip install --requirement ${BASE_DIR}/requirements.txt --requirement ${BASE_DIR}/manager_requirements.txt --extra-index-url https://download.pytorch.org/whl/${CUDA_VERSION}
+  ${HOME}/.venv/bin/pip install --requirement ${BASE_DIR}/requirements.txt --requirement ${BASE_DIR}/manager_requirements.txt --extra-index-url https://download.pytorch.org/whl/${CUDA_VERSION}
 else
-  ${BASE_DIR}/.venv/bin/pip install --requirement ${BASE_DIR}/requirements.txt --requirement ${BASE_DIR}/manager_requirements.txt
+  ${HOME}/.venv/bin/pip install --requirement ${BASE_DIR}/requirements.txt --requirement ${BASE_DIR}/manager_requirements.txt
 fi  
 
-${BASE_DIR}/.venv/bin/pip install matrix-nio
+${HOME}/.venv/bin/pip install matrix-nio
 
 if [[ $PERSONAL_CLOUD == 'true' ]]
 then
-  sed -i "s/network_mode = public/network_mode = personal_cloud/" ComfyUI/user/__manager/config.ini 
+  sed -i "s/network_mode = public/network_mode = personal_cloud/" ${BASE_DIR}/user/__manager/config.ini 
 fi
 
 if [[ $GPU_TYPE == 'amd' ]] || [[ $GPU_TYPE == 'nvidia' ]]
 then
-    ${BASE_DIR}/.venv/bin/python ${BASE_DIR}/main.py \
+    ${HOME}/.venv/bin/python ${BASE_DIR}/main.py \
         --enable-cors-header \
         --disable-auto-launch \
         ${CUSTOM_TEMP} \
@@ -49,7 +42,7 @@ then
         --listen 0.0.0.0 \
         --port 8188
 else
-    ${BASE_DIR}/.venv/bin/python ${BASE_DIR}/main.py \
+    ${HOME}/.venv/bin/python ${BASE_DIR}/main.py \
         --enable-cors-header \
         --disable-auto-launch \
         ${CUSTOM_TEMP} \
