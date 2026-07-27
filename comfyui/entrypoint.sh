08d58a2a3b10ce5600e8987756d6a4a962548043
Manpreet Singh Nehra <manpreet@nehraconsultancy.net>
2026-07-27 11:05:41 +0300

    No code just prep for sync

--- comfyui/entrypoint.sh
+++ comfyui/entrypoint.sh
@@ -1,21 +1,6 @@
 #!/usr/bin/bash -x
 
-BASE_DIR=/home/comfy/ComfyUI-master
-
-if [[ -z "${TEMP_DIR}" ]];then TEMP_DIR="${BASE_DIR}/temp"; fi
-if [[ -z "${INPUT_DIR}" ]];then INPUT_DIR="${BASE_DIR}/input";fi
-if [[ -z "${OUTPUT_DIR}" ]];then OUTPUT_DIR="${BASE_DIR}/output";fi
-if [[ -z "${USER_DIR}" ]];then USER_DIR="${BASE_DIR}/user";fi
-
-mkdir $USER_DIR
-
-CUSTOM_TEMP="--temp-directory $TEMP_DIR"
-CUSTOM_INPUT="--input-directory $INPUT_DIR"
-CUSTOM_OUTPUT="--output-directory $OUTPUT_DIR"
-CUSTOM_USER="--user-directory $USER_DIR"
-
-### Set Vars
-if [[ -z "${BASE_DIR}" ]]; then BASE_DIR=/home/comfy/ComfyUI-master; fi
+BASE_DIR=/home/comfy/ComfyUI
 
 ## Setup Virtual Env
 virtualenv ${HOME}/.venv
@@ -44,10 +29,6 @@ then
     ${HOME}/.venv/bin/python ${BASE_DIR}/main.py \
         --enable-cors-header '*' \
         --disable-auto-launch \
-        ${CUSTOM_TEMP} \
-        ${CUSTOM_INPUT} \
-        ${CUSTOM_OUTPUT} \
-        ${CUSTOM_USER} \
         --enable-manager \
         --listen 0.0.0.0 \
         --port 8188
@@ -55,10 +36,6 @@ else
     ${HOME}/.venv/bin/python ${BASE_DIR}/main.py \
         --enable-cors-header '*' \
         --disable-auto-launch \
-        ${CUSTOM_TEMP} \
-        ${CUSTOM_INPUT} \
-        ${CUSTOM_OUTPUT} \
-        ${CUSTOM_USER} \
         --enable-manager \
         --cpu \
         --listen 0.0.0.0 \
