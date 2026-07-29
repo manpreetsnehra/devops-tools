30310aa8176e9321e4a3cd15456d436d59b5bfdd
Manpreet Singh Nehra <manpreet@nehraconsultancy.net>
2026-07-29 10:57:58 +0300

    Container image/entrypoint update

--- comfyui/entrypoint.sh
+++ comfyui/entrypoint.sh
@@ -5,45 +5,54 @@ BASE_DIR=/home/comfy/ComfyUI
 ## Setup Virtual Env
 virtualenv ${HOME}/.venv
 
+## Download and Extract ComfyUI
+if [[ $INSTALL_COMFYUI == "true" ]] || [[ ! -d "${BASE_DIR}/.git" ]]
+then
+  git clone https://github.com/Comfy-Org/ComfyUI.git
+fi
+
+if [[ $TYPE != "worker" ]]
+then
 ## Install Pytorch
-if [[ $GPU_TYPE == 'amd' ]]
-then
-  ${HOME}/.venv/bin/pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm${ROCM_VERSION}
-  ${HOME}/.venv/bin/pip install -r ${BASE_DIR}/requirements.txt -r ${BASE_DIR}/manager_requirements.txt matrix-nio huggingface_hub
-elif [[ $GPU_TYPE == 'nvidia' ]]
-then
-  ${HOME}/.venv/bin/pip install -r ${BASE_DIR}/requirements.txt -r ${BASE_DIR}/manager_requirements.txt matrix-nio huggingface_hub --extra-index-url https://download.pytorch.org/whl/${CUDA_VERSION}
-else
-  ${HOME}/.venv/bin/pip install -r ${BASE_DIR}/requirements.txt -r ${BASE_DIR}/manager_requirements.txt matrix-nio huggingface_hub
-fi  
+  if [[ $GPU_TYPE == 'amd' ]]
+  then
+    ${HOME}/.venv/bin/pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm${ROCM_VERSION}
+    ${HOME}/.venv/bin/pip install -r ${BASE_DIR}/requirements.txt -r ${BASE_DIR}/manager_requirements.txt matrix-nio huggingface_hub
+  elif [[ $GPU_TYPE == 'nvidia' ]]
+  then
+    ${HOME}/.venv/bin/pip install -r ${BASE_DIR}/requirements.txt -r ${BASE_DIR}/manager_requirements.txt matrix-nio huggingface_hub --extra-index-url https://download.pytorch.org/whl/${CUDA_VERSION}
+  else
+    ${HOME}/.venv/bin/pip install -r ${BASE_DIR}/requirements.txt -r ${BASE_DIR}/manager_requirements.txt matrix-nio huggingface_hub
+  fi  
 
-for dir in ${BASE_DIR}/custom_nodes/*
-do
-  if [[ -f ${dir}/requirements.txt ]]
-  then 
-    ${HOME}/.venv/bin/pip install -r ${dir}/requirements.txt
+  for dir in ${BASE_DIR}/custom_nodes/*
+  do
+    if [[ -f ${dir}/requirements.txt ]]
+    then 
+      ${HOME}/.venv/bin/pip install -r ${dir}/requirements.txt
+    fi
+  done
+
+  if [[ $PERSONAL_CLOUD == 'true' ]] && [[ -f "${BASE_DIR}/user/__manager/config.ini" ]]
+  then
+    sed -i "s/network_mode = public/network_mode = personal_cloud/" ${BASE_DIR}/user/__manager/config.ini 
   fi
-done
-
-if [[ $PERSONAL_CLOUD == 'true' ]]
-then
-  sed -i "s/network_mode = public/network_mode = personal_cloud/" ${BASE_DIR}/user/__manager/config.ini 
 fi
 
 if [[ $GPU_TYPE == 'amd' ]] || [[ $GPU_TYPE == 'nvidia' ]]
 then
     ${HOME}/.venv/bin/python ${BASE_DIR}/main.py \
-        --enable-cors-header "'*'" \
+        --enable-cors-header '*' \
         --disable-auto-launch \
         --enable-manager \
         --listen 0.0.0.0 \
-        --port 8188
+        --port 8188 ${COMFYUI_ARGS}
 else
     ${HOME}/.venv/bin/python ${BASE_DIR}/main.py \
-        --enable-cors-header "'*'" \
+        --enable-cors-header '*' \
         --disable-auto-launch \
         --enable-manager \
         --cpu \
         --listen 0.0.0.0 \
-        --port 8188
+        --port 8188 ${COMFYUI_ARGS}
 fi
